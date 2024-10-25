9a04af1a02f8f9b0019b6e9eb78650e1dff0d97d
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-10-25 17:47:51 +0100

    Refactor font import in layout.tsx to use Plus_Jakarta_Sans instead of Josefin_Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "@/app/_styles/globals.css";
-import { Josefin_Sans} from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
-const josefin = Josefin_Sans({
+const jakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
   display: "swap",
 });
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+        className={`${jakartaSans.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         {children}
       </body>
