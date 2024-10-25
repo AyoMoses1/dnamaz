f0d6451b753df12d877b4d1139c2aaba7ac23703
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-10-25 08:59:15 +0100

    Refactor Header component to display full name, add spacing, and include notification bell

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,6 +1,6 @@
-// components/Header.tsx
 import Image from "next/image";
 import { FC } from "react";
+import { FaBell } from "react-icons/fa";
 import rita from "@/public/Rita.jpg";
 
 interface HeaderProps {
@@ -10,21 +10,30 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ name, email }) => {
   return (
-    <header className="p-5 flex justify-between items-center">
+    <header className="pt-5 flex justify-between items-center">
       <div>
-        <h1 className="text-gray-500 text-2xl font-bold">Hello, {name}!</h1>
+        <h1 className="text-gray-700 text-2xl font-bold">Hello, {name}!</h1>
         <p className="text-gray-500">Here’s an overview of your business!</p>
       </div>
-      <div className="flex items-center space-x-3">
-        <Image
-          src={rita}
-          alt="User Avatar"
-          quality={80}
-          className="w-12 h-12 rounded-full"
-        />
-        <div>
-          <p className="text-gray-500">{name}</p>
-          <p className="">{email}</p>
+      <div className="flex items-center space-x-5">
+        <div className="relative">
+          <div className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full">
+            <FaBell className="text-gray-500 text-lg" />
+          </div>
+          <span className="absolute top-0 right-0 w-3 h-3 bg-red-500 rounded-full"></span>
+        </div>
+       
+        <div className="flex items-center space-x-3">
+          <Image
+            src={rita}
+            alt="User Avatar"
+            quality={80}
+            className="w-12 h-12 rounded-full"
+          />
+          <div>
+            <p className="text-gray-700 font-bold">{name}</p>
+            <p className="text-gray-500">{email}</p>
+          </div>
         </div>
       </div>
     </header>
