b7773aced019cf2c1380ba7b1d157aeb7515d4a7
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-11-06 21:12:11 +0100

    Add funding page with Header, Sidebar, OutstandingBalance, and FundingTable components

---
+++ app/funding/page.tsx
@@ -0,0 +1,35 @@
+import Header from "@/app/_components/Header";
+import Sidebar from "@/app/_components/Sidebar";
+import OutstandingBalance from "@/app/_components/OutstandingBalance";
+import FundingTable from "../_components/FundingTable";
+
+export default function Page() {
+  return (
+    <div className="flex">
+      <div className="fixed">
+         <Sidebar/>
+      </div>
+     <main className="ml-64 flex-1 p-6">
+      <div className="flex-1 p-5">
+        <Header 
+          name="Asset Finance" 
+          description="Here's an overview of your funded project base" 
+          email="adamu.tosin@gmail.com" />
+
+    <div className="container mx-auto py-8">
+      <OutstandingBalance
+        totalFundedProjects={713}
+        outstandingBalance={9500000}
+        dueAmount={500000}
+        dueClients={21}
+        cooperateCustomers={69}
+        individualCustomers={0}
+      />
+    </div>
+
+        <FundingTable />
+      </div>
+    </main>
+    </div>
+  );
+}
