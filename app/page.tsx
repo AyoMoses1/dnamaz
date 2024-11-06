32f997510bba9b12772d67ed7f6f2799435480ba
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-11-06 21:11:04 +0100

    Refactor admin page to implement login form and update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,31 @@
-import Sidebar from "@/app/_components/Sidebar";
-import Header from "@/app/_components/Header";
-import OverviewCards from "@/app/_components/OverviewCards";
-import NewAccountsTable from "@/app/_components/NewAccountsTable";
+import LoginForm from "@/app/_components/LoginForm";
+import Image from "next/image";
+import dlogo from "@/public/dlogo.png";
+import { LogOut } from "lucide-react";
 
-const Page = () => {
+export default function Page() {
   return (
-    <div className="flex">
-      <div className="fixed">
-         <Sidebar/>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="absolute top-10 left-10">
+        <Image src={dlogo} alt="D'namaz Capital Limited" width={120} height={120} />
       </div>
-     <main className="ml-64 flex-1 p-6">
-      <div className="flex-1 p-5">
-        <Header name="Hello, Adamu Tosin" title="business!" description="Here’s an overview of your business" email="adamu.tosin@gmail.com" />
-        <div className="mb-6"></div>
-        <OverviewCards />
-        <NewAccountsTable />
+
+      <h2 className="text-3xl font-bold text-green-800 mb-6 text-center">
+        Admin Control Panel
+      </h2>
+
+      <div className="bg-white rounded-xl shadow-md p-8 w-full max-w-md flex flex-col items-center">
+        <div className="bg-white border p-4 rounded-md mb-4">
+          <LogOut className="text-black w-10 h-10" />
+        </div>
+
+        <h3 className="text-xl font-semibold text-black mb-2 text-center">Sign in with username</h3>
+        <p className="text-gray-500 text-center mb-6 text-sm">
+          Verify account, Get business overview and Approve request
+        </p>
+
+        <LoginForm />
       </div>
-    </main>
     </div>
   );
-};
-
-export default Page;
+}
