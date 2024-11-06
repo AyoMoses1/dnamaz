d529ce67de3aeaa52e2c44d8cb33e423ac54c5ca
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-11-06 21:12:21 +0100

    Add dashboard page with Sidebar, Header, OverviewCards, and NewAccountsTable components

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import Sidebar from "@/app/_components/Sidebar";
+import Header from "@/app/_components/Header";
+import OverviewCards from "@/app/_components/OverviewCards";
+import NewAccountsTable from "@/app/_components/NewAccountsTable";
+
+const Page = () => {
+  return (
+    <div className="flex">
+      <div className="fixed">
+         <Sidebar/>
+      </div>
+     <main className="ml-64 flex-1 p-6">
+      <div className="flex-1 p-5">
+        <Header name="Hello, Adamu Tosin" title="business!" description="Here’s an overview of your business" email="adamu.tosin@gmail.com" />
+        <div className="mb-6"></div>
+        <OverviewCards />
+        <NewAccountsTable />
+      </div>
+    </main>
+    </div>
+  );
+};
+
+export default Page;
