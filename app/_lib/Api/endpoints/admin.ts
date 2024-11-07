12c5cb09ec50775a0efccaf49f26ad86b203d4e5
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-11-07 20:18:11 +0100

    feat: implement adminLogin function to use guestPost for API requests and improve error handling

--- app/_lib/Api/endpoints/admin.ts
+++ app/_lib/Api/endpoints/admin.ts
@@ -1,22 +1,16 @@
-import axiosInstance from '../axiosInstance';
+import { guestPost } from '../axiosGuest';
 import { AdminLoginRequest, AdminLoginResponse } from '@/app/_types/apiTypes';
+import { handleAxiosError } from '@/app/utils/errorHandler';
+import { ADMIN_LOGIN_ENDPOINT } from '..';
 
 export const adminLogin = async (
   loginData: AdminLoginRequest
-): Promise<AdminLoginResponse> => {
+): Promise<AdminLoginResponse | undefined> => {
   try {
-    const response = await axiosInstance.post<AdminLoginResponse>('/api/v1/admin/login', loginData);
-    return response.data;
+    const response = await guestPost(ADMIN_LOGIN_ENDPOINT, loginData);
+    return response;
   } catch (error: any) {
-    if (error.response) {
-      console.error('Login error:', error.response.data);
-      throw new Error(error.response.data.message || 'Login failed');
-    } else if (error.request) {
-      console.error('No response received from server:', error.request);
-      throw new Error('Network error: No response received from the server. Please check your connection.');
-    } else {
-      console.error('Error during request:', error.message);
-      throw new Error('Unexpected error: ' + error.message);
-    }
+    handleAxiosError(error); 
+    throw error; 
   }
 };
