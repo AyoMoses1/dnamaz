773193111715d4c0aad896d0f52a0438c5542bbd
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-11-10 22:56:19 +0100

    feat: add Spinner component for loading indication

---
+++ app/_components/Spinner.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="flex justify-center items-center">
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-green-900"></div>
+  </div>
+);
+
+export default Spinner;
