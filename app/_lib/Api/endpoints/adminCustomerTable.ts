04856b1972b0e14d0beda781e4f28771e430b50a
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-11-10 22:56:54 +0100

    feat: implement fetchCustomers function for customer data retrieval

---
+++ app/_lib/Api/endpoints/adminCustomerTable.ts
@@ -0,0 +1,29 @@
+import { ADMIN_CUSTOMER_ENDPOINT } from ".."; 
+import { CustomerResponse, CustomerError } from "@/app/_types/apiTypes";
+import { handleAxiosError } from "@/app/utils/errorHandler"; 
+import { guestGet } from "../axiosGuest";
+
+interface fetchCustomersParams {
+    user_type: string;  
+    page?: number;
+    page_size?: number;
+    search?: string;
+}
+
+export const fetchCustomers = async (
+  params: fetchCustomersParams
+): Promise<CustomerResponse> => {
+  try {
+    const response = await guestGet(ADMIN_CUSTOMER_ENDPOINT, {
+      
+        user_type: params.user_type.toLowerCase(),  
+        page: params.page || 1,
+        page_size: params.page_size,
+        search: params.search || "",
+ 
+    });
+    return response;
+  } catch (error) {
+    return handleAxiosError(error);
+  }
+};
