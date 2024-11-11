962058d429ed45bb9ddb73346494503ee9548515
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-11-11 09:12:37 +0100

    fix: remove console log from new customer request API endpoint

--- app/_lib/Api/endpoints/newCustomers.ts
+++ app/_lib/Api/endpoints/newCustomers.ts
@@ -1,13 +1,11 @@
 import { guestGet } from "../axiosGuest";
-import { NewRequest } from "@/app/_types/apiTypes";
 import { handleAxiosError } from "@/app/utils/errorHandler";
 import { NEW_ACCOUNT_REQUEST_ENDPOINT } from "..";
 
 export const newCustomerRequest = async () => {
   try {
     const response = await guestGet(NEW_ACCOUNT_REQUEST_ENDPOINT);
-    console.log("API Response:", response);
-    return response; // Make sure the structure of 'response' matches what you're expecting
+    return response; 
   } catch (error) {
     handleAxiosError(error);
     throw error;
