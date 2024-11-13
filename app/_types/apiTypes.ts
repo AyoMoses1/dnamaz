be04c7434f4f50cde1f8f8d0658c83e4e6918f88
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-11-13 04:24:59 +0100

    feat: add CooperateProfile and IndividualProfile interfaces to apiTypes

--- app/_types/apiTypes.ts
+++ app/_types/apiTypes.ts
@@ -2,8 +2,6 @@ export interface AdminLoginRequest {
   username: string;
   password: string;
 }
-
-
 export interface AdminLoginResponse {
   token: {
     type: string; 
@@ -32,9 +30,6 @@ export interface NewRequest {
   }[];
   message: string;
 }
-
-// types.ts
-
 export interface Customer {
   userId: number;
   email: string;
@@ -65,5 +60,70 @@ export interface CustomerResponse {
 
 export interface CustomerError {
   error: string;
+  message: string;
+}
+
+export interface CooperateProfile {
+  id: number;
+  rc: string;
+  dateOfIncorporation: string;
+  businessName: string;
+  natureOfBusiness: string;
+  primaryBusinessAddress: string;
+  phone: string;
+  companyEmail: string;
+  activationStatus: string;
+  kycLevel: number;
+  cooperateRepresentative: {
+    id: number;
+    fullName: string;
+    designation: string;
+    idNumber: string;
+    issueDate: string;
+    expiryDate: string;
+    document: string;
+    typeOfId: string;
+  };
+  cooperateDocuments: Array<{
+    id: number;
+    typeOfId: string;
+    document: string;
+    idNumber: string | null;
+    issueDate: string | null;
+    expiryDate: string | null;
+    documentType: string;
+  }>;
+}
+
+export interface IndividualProfile {
+  id: number;
+  firstName: string;
+  lastName: string;
+  clientNumber: string;
+  occupation: string;
+  houseAddress: string;
+  phoneNumber: string;
+  nationality: string;
+  dateOfBirth: string;
+  userDocument?: Array<{
+    id: number;
+    typeOfId: string;
+    document: string;
+    documentType: string;
+  }>;
+  nextOfKin?: {
+    firstName: string;
+    lastName: string;
+    relationship: string;
+    email: string;
+  };
+}
+
+export interface ClientResponse {
+  data: {
+    userId: number;
+    userType: "cooperate" | "individual";
+    profile: CooperateProfile | IndividualProfile;
+  };
   message: string;
 }
