a9d2fc72726491dd717dd443941de9253a826ca6
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-12-02 22:53:56 +0100

    feat: add ADMIN_CUSTOMER_UPDATE_REQUEST_ENDPOINT for customer update functionality

--- app/_lib/Api/index.ts
+++ app/_lib/Api/index.ts
@@ -1,3 +1,4 @@
 export const ADMIN_LOGIN_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/login`;
 export const NEW_ACCOUNT_REQUEST_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/recent`;
 export const ADMIN_CUSTOMER_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/customer`;
+export const ADMIN_CUSTOMER_UPDATE_REQUEST_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/customer/update`;
