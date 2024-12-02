29c28ff1a497bbed539b6cc6509252d4f30128f5
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-12-02 10:32:59 +0100

    feat: integrate AuthSessionProvider and AxiosAuthUserProvider into Providers component

--- app/_lib/_providers/index.tsx
+++ app/_lib/_providers/index.tsx
@@ -1,12 +1,20 @@
 import QueryProvider from "@/app/_lib/_providers/QueryProvider";
 import AxiosGuestProvider from "../Api/axiosGuest";
+import AuthSessionProvider from "./AuthSessionProvider";
 import { Query } from "@tanstack/react-query";
+import AxiosAuthUserProvider from "../Api/axiosAuthUser";
+import { auth } from "../nextauth/auth";
 
-const Providers = ({ children }: { children: React.ReactNode }) => {
+const Providers = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+
   return (
     <QueryProvider>
-      <AxiosGuestProvider />
+      <AuthSessionProvider session={session}>
+        <AxiosGuestProvider />
+        <AxiosAuthUserProvider />
         {children}
+      </AuthSessionProvider>
     </QueryProvider>
   );
 };
