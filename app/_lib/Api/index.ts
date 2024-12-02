7126c646875bfd8ee56114196875a8b1e93db55d
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-12-02 10:33:45 +0100

    feat: update API endpoint constants to use environment variable for base URL

--- app/_lib/Api/index.ts
+++ app/_lib/Api/index.ts
@@ -1,4 +1,3 @@
-export const ADMIN_LOGIN_ENDPOINT = '/api/v1/admin/login';
-
-export const NEW_ACCOUNT_REQUEST_ENDPOINT = '/api/v1/admin/recent';
-export const ADMIN_CUSTOMER_ENDPOINT = '/api/v1/admin/customer';
+export const ADMIN_LOGIN_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/login`;
+export const NEW_ACCOUNT_REQUEST_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/recent`;
+export const ADMIN_CUSTOMER_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/customer`;
