dfcdd1dec454cbc0d73a2be6a8582f96c19a316b
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-12-02 23:16:00 +0100

    refactor: clean up JSX formatting and improve component structure in funding page

--- app/funding/page.tsx
+++ app/funding/page.tsx
@@ -7,29 +7,30 @@ export default function Page() {
   return (
     <div className="flex">
       <div className="fixed">
-         <Sidebar/>
+        <Sidebar />
       </div>
-     <main className="ml-64 flex-1 p-6">
-      <div className="flex-1 p-5">
-        <Header 
-          name="Asset Finance" 
-          description="Here's an overview of your funded project base" 
-          email="adamu.tosin@gmail.com" />
+      <main className="ml-64 flex-1 p-6">
+        <div className="flex-1 p-5">
+          <Header
+            name="Asset Finance"
+            description="Here's an overview of your funded project base"
+            email="adamu.tosin@gmail.com"
+          />
 
-    <div className="container mx-auto py-8">
-      <OutstandingBalance
+          <div className="container mx-auto py-8">
+            {/* <OutstandingBalance
         totalProjects={713}
         outstandingBalance={9500000}
         dueAmount={500000}
         dueClients={21}
         cooperateCustomers={69}
         individualCustomers={0}
-      />
-    </div>
+      /> */}
+          </div>
 
-        <FundingTable />
-      </div>
-    </main>
+          <FundingTable />
+        </div>
+      </main>
     </div>
   );
 }
