b2cb6fe976de39562c037bcb416948e3cb47c434
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-12-12 10:21:06 +0100

    feat: add TOTAL_USERS_ENDPOINT for user statistics retrieval

--- app/_lib/Api/index.ts
+++ app/_lib/Api/index.ts
@@ -4,3 +4,4 @@ export const ADMIN_CUSTOMER_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v
 export const ADMIN_CUSTOMER_UPDATE_REQUEST_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/customer/update`;
 export const WALLETSTATS_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/customer/walletstats`;
 export const STAFF_PROFILE_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/staff/me`;
+export const TOTAL_USERS_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/customer/userstats`;
